13ea507bf0f6510f2d93de6d63d84c6038d8cb8d
shubhra srivastava <shubhra@mamba.local>
2020-03-30 23:38:19 +0530

    tmpartner form

---
+++ onboarding/onboarding/const.py
@@ -0,0 +1,2 @@
+LANGUAGE_CHOICE = ["English", "Hindi", "Tamil", "Kannada", "Bengali", "Malayalam", "Oriya", "Telugu", "Marathi", "Gujarati", "Assamese", "Punjabi", "Other"]
+DEDICATE_HOURS_CHOICE = ["2", "4", "6", ">8", "Other"]
